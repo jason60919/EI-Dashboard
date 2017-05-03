6a4d1dd25c1b89580dec3c400ac2175f1b20f862
Dylan.Chang <dylan.chang@advantech.com.tw>
2017-05-03 18:06:03 +0800

    [WISE-PaaS V2 ES-Dashboard] [UI]   1 : Modify redirect url of III SSO

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -228,7 +228,7 @@ $(function () {
 
     $('#frmLogin_btnoAzureIIILogin').click(function () {
         debugger;
-        var redirectUrl = 'https://login.windows.net/c53d73cb-64c4-4c1d-b972-7f92d1330c39/oauth2/authorize?response_type=code&redirect_uri=' + GLOBAL_CONFIG.hostUrl + '/web/redirectPage.html&client_id=09ea49b3-09fc-4b9a-b452-52563e9d4add&state=' + GLOBAL_CONFIG.hostUrl + '/index.html';
+        var redirectUrl = 'https://login.windows.net/c53d73cb-64c4-4c1d-b972-7f92d1330c39/oauth2/authorize?response_type=code&redirect_uri=' + GLOBAL_CONFIG.hostUrl + '/index.html&client_id=09ea49b3-09fc-4b9a-b452-52563e9d4add&state=' + GLOBAL_CONFIG.hostUrl + '/index.html';
         window.location.href = redirectUrl;
     });
 
