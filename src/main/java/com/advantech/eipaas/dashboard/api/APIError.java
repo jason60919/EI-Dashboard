d3fe136925fc969ca18cb2890e1efb559d42e541
Alex Shao <alex.shao@advantech.com.tw>
2017-06-21 17:23:02 +0800

    reformat code and add more error for sso and system builtin

--- src/main/java/com/advantech/eipaas/dashboard/api/APIError.java
+++ src/main/java/com/advantech/eipaas/dashboard/api/APIError.java
@@ -7,44 +7,60 @@ import java.util.Map;
 
 public enum APIError {
     ServerError(
-        1000, "E_SERVER_INTERNAL",
-        "server encountered internal problem"
+            1000, "E_SERVER_INTERNAL",
+            "server encountered internal problem"
     ),
     AuthError(
-        1001, "E_AUTH",
-        "authentication/authorization error"
+            1001, "E_AUTH",
+            "authentication/authorization error"
     ),
     AccountNotEnabledError(
-        1002, "E_ACCOUNT_NOT_ENABLED",
-        "given account not enabled in this system"
+            1002, "E_ACCOUNT_NOT_ENABLED",
+            "given account not enabled in this system"
     ),
     DataNotCompleteError(
-        1003, "E_DATA_NOT_COMPLETE",
-        "given data missing specific attribute(s)"
+            1003, "E_DATA_NOT_COMPLETE",
+            "given data missing specific attribute(s)"
     ),
     DatabaseOperationError(
-        1004, "E_DATABASE_OPERATION",
-        "server encountered problem during database operation"
+            1004, "E_DATABASE_OPERATION",
+            "server encountered problem during database operation"
     ),
     SheetNotFoundError(
-        1005, "E_SHEET_NOT_FOUND",
-        "given sheet can't be found in database"
+            1005, "E_SHEET_NOT_FOUND",
+            "given sheet can't be found in database"
     ),
     SequenceOutOfBoundError(
-        1006, "E_Sequence_OUT_OF_BOUND",
-        "given sequence value is out of bound"
+            1006, "E_Sequence_OUT_OF_BOUND",
+            "given sequence value is out of bound"
     ),
     AuthNotProvidedError(
-        1007, "E_AUTH_NOT_PROVIDED",
-        "authentication/authorization data not provided in header"
+            1007, "E_AUTH_NOT_PROVIDED",
+            "authentication/authorization data not provided in header"
     ),
     AuthNotSupportedError(
-        1008, "E_AUTH_NOT_SUPPORTED",
-        "authentication/authorization type not supported in header"
+            1008, "E_AUTH_NOT_SUPPORTED",
+            "authentication/authorization type not supported in header"
     ),
     AuthDataError(
-        1009, "E_AUTH_DATA",
-        "authentication/authorization data provided in header error"
+            1009, "E_AUTH_DATA",
+            "authentication/authorization data provided in header error"
+    ),
+    AuthSSOValidationError(
+            1010, "E_AUTH_SSO_VALIDATION",
+            "sso token validation error"
+    ),
+    AuthSSORefreshError(
+            1011, "E_AUTH_SSO_REFRESH",
+            "sso token refresh error"
+    ),
+    AuthTokenVerificationError(
+            1012, "E_AUTH_TOKEN_VERIFY",
+            "jwt token verification error"
+    ),
+    AuthTokenExpiredError(
+            1013, "E_AUTH_TOKEN_EXPIRED",
+            "jwt token expired"
     );
 
     private int code;
