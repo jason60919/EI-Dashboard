27aba3be6869785ee6a362cbba39520f1a04f715
Alex Shao <alex.shao@advantech.com.tw>
2017-06-21 17:24:57 +0800

    change APIException usage from returning Response to ResponseBuilder

--- src/main/java/com/advantech/eipaas/dashboard/api/APIException.java
+++ src/main/java/com/advantech/eipaas/dashboard/api/APIException.java
@@ -2,13 +2,14 @@ package com.advantech.eipaas.dashboard.api;
 
 
 public class APIException extends Exception {
-    private javax.ws.rs.core.Response errorResponse;
+    private javax.ws.rs.core.Response.ResponseBuilder errorResponse;
 
-    public javax.ws.rs.core.Response getErrorResponse() {
+    public javax.ws.rs.core.Response.ResponseBuilder getErrorResponse() {
         return this.errorResponse;
     }
 
-    public APIException(javax.ws.rs.core.Response errorResponse) {
+    public APIException(
+            javax.ws.rs.core.Response.ResponseBuilder errorResponse) {
         super();
         this.errorResponse = errorResponse;
     }
