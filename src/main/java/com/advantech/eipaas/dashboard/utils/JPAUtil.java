7dde082413bce731d30faa694f73760be99762c0
Alex Shao <alex.shao@advantech.com.tw>
2017-06-21 17:23:54 +0800

    apply jsonpath to JPAUtil for extract environments from PCF with ease

--- src/main/java/com/advantech/eipaas/dashboard/utils/JPAUtil.java
+++ src/main/java/com/advantech/eipaas/dashboard/utils/JPAUtil.java
@@ -2,7 +2,7 @@ package com.advantech.eipaas.dashboard.utils;
 
 
 import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,9 +10,6 @@ import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-import org.json.JSONObject;
-import org.json.JSONArray;
 
 
 @WebListener
@@ -22,13 +19,14 @@ public class JPAUtil implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent event) {
         // Acquire environment variables from PCF
-        Map<String, Object> properties = getPropertiesFromPivotalEnv();
-        if (properties.isEmpty()) {
+        List<Map<String, Object>> properties = PCFUtil.getEnvFromVcapServices(
+                "$.user-provided[?(@.name=='PSQL-Dashboard')].credentials"
+        );
+        if (0 == properties.size()) {
             emf = Persistence.createEntityManagerFactory("DashboardPU");
-        }
-        else {
+        } else {
             emf = Persistence.createEntityManagerFactory(
-                "DashboardPU", properties
+                    "DashboardPU", properties.get(0)
             );
         }
     }
@@ -45,49 +43,5 @@ public class JPAUtil implements ServletContextListener {
             throw new IllegalStateException("Context is not initialized yet.");
         }
         return emf.createEntityManager();
-    }
-
-    private Map<String, Object> getPropertiesFromPivotalEnv() {
-        Map<String, Object> env = new HashMap<>();
-
-        String VCAP_SERVICES = System.getenv("VCAP_SERVICES");
-        if (null != VCAP_SERVICES && VCAP_SERVICES.length() > 0) {
-            JSONObject vcap_services = new JSONObject(VCAP_SERVICES);
-            if (vcap_services.has("user-provided")) {
-                JSONArray jsons = vcap_services.getJSONArray("user-provided");
-                for (int i = 0; i < jsons.length(); i++) {
-                    JSONObject json = jsons.getJSONObject(i);
-                    if (!json.has("name") || !json.has("credentials"))
-                        continue;
-
-                    String name = json.getString("name");
-                    if (name.equals("PSQL-Dashboard")) {
-                        JSONObject credentials = json.getJSONObject(
-                            "credentials");
-                        if (credentials.has("url")) {
-                            env.put(
-                                "hibernate.connection.url",
-                                credentials.getString("url")
-                            );
-                        }
-                        if (credentials.has("username")) {
-                            env.put(
-                                "hibernate.connection.username",
-                                credentials.getString("username")
-                            );
-                        }
-                        if (credentials.has("password")) {
-                            env.put(
-                                "hibernate.connection.password",
-                                credentials.getString("password")
-                            );
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-
-        return env;
     }
 }
