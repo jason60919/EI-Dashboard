39ac1f8108151311581070e7a60fbe1aa972d14b
Alex Shao <alex.shao@advantech.com.tw>
2017-06-21 17:27:12 +0800

    Use ResponseBuilder instead of Response in APIResponse

--- src/main/java/com/advantech/eipaas/dashboard/api/APIResponse.java
+++ src/main/java/com/advantech/eipaas/dashboard/api/APIResponse.java
@@ -4,27 +4,31 @@ package com.advantech.eipaas.dashboard.api;
 import java.util.Map;
 import java.util.HashMap;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import org.json.JSONObject;
 
 
 public class APIResponse {
-    private Response makeResponse(Response.Status status, Object result) {
+    private ResponseBuilder makeResponse(Response.Status status,
+                                         Object result) {
         JSONObject json = new JSONObject();
         json.put("success", javax.ws.rs.core.Response.Status.OK == status);
         json.put("result", null == result ? "Unknown" : result);
-        return Response.status(status).entity(json.toString()).build();
+        return Response.status(status).entity(json.toString());
     }
 
-    public Response success(Object data) {
+    public ResponseBuilder success(Object data) {
         return makeResponse(Response.Status.OK, data);
     }
 
-    public Response success(Response.Status status, Object data) {
+    public ResponseBuilder success(Response.Status status, Object data) {
         return makeResponse(status, data);
     }
 
-    public Response fail(Response.Status status, int errCode, String detail) {
+    public ResponseBuilder fail(Response.Status status,
+                                int errCode,
+                                String detail) {
         APIError e = APIError.getErrorInstance(errCode);
 
         Map<String, Object> result = new HashMap<>();
@@ -38,7 +42,7 @@ public class APIResponse {
         return makeResponse(status, result);
     }
 
-    public Response fail(Response.Status status, int errorCode) {
+    public ResponseBuilder fail(Response.Status status, int errorCode) {
         return fail(status, errorCode, null);
     }
 }
